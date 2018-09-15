92707599867cddbe95eb5de163f2d59eca5756c7
Shade Wilson <shadew@uw.edu>
2018-09-15 14:40:31 -0700

    Implement add and subtract methods for Letter Inventories

--- inventory.py
+++ inventory.py
@@ -9,14 +9,32 @@ class LetterInventory:
 				self.inventory[letter] += 1
 			else:
 				self.inventory[letter] = 1
+			self.size += 1
+
 		print("Inventory created: {}".format(self.__str__()))
 
 	def __str__(self):
 		str = ""
 		for k, v in self.inventory.items():
 			str += k * v
-		return str
+
+		return str + ", size {}".format(self.size)
 
 
-	#def add()
+	def add(self, other):
+		for letter, v in other.inventory.items():
+			if letter in self.inventory:
+				self.inventory[letter] += v
+			else:
+				self.inventory[letter] = v
+			self.size += 1
+
+
+	def subtract(self, other):
+		for letter, v in other.inventory.items():
+			if letter in self.inventory:
+				self.inventory[letter] -= v
+				if self.inventory[letter] < 0:
+					self.inventory[letter] = 0
+				self.size -= 1
 
