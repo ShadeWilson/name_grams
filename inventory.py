ec57b3d6e8b5c7acd50a6a201085f008fe85fe2d
Shade Wilson <shadew@uw.edu>
2018-09-15 15:16:12 -0700

    Convert add and subtract methods to nonmutating

--- inventory.py
+++ inventory.py
@@ -16,37 +16,43 @@ class LetterInventory:
 				self.inventory[letter] = 1
 			self.size += 1
 
-		print("Inventory created: {}".format(self.__str__()))
+		#print("Inventory created: {}".format(self.__str__()))
 
 	def __str__(self):
 		str = ""
 		for k, v in self.inventory.items():
 			str += k * v
 
-		return str + ", size {}".format(self.size)
+		return str
 
 
 	def add(self, other):
+		new = self.copy()
 		for letter, v in other.inventory.items():
-			if letter in self.inventory:
-				self.inventory[letter] += v
+			if letter in new.inventory:
+				new.inventory[letter] += v
 			else:
-				self.inventory[letter] = v
-			self.size += v
+				new.inventory[letter] = v
+			new.size += v
+		return new
 
 
 	def subtract(self, other):
+		new = self.copy()
 		for letter, v in other.inventory.items():
-			if letter in self.inventory:
-				self.inventory[letter] -= v
-				self.size -= v
-				new_count = self.inventory[letter]
+			if letter in new.inventory:
+				new.inventory[letter] -= v
+				new.size -= v
+				new_count = new.inventory[letter]
 				if new_count < 0:
-					self.inventory[letter] = 0
-					self.size -= new_count
+					new.inventory[letter] = 0
+					new.size -= new_count
+		return new
 
 
 	def is_empty(self):
 		return self.size == 0
 				
 
+	def copy(self):
+		return LetterInventory(self.__str__())
