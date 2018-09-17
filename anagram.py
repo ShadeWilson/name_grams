f4f0c31682ba7d1ae823f8feed8a039a1553bcfa
Shade Wilson <shadew@uw.edu>
2018-09-16 23:27:16 -0700

    Recursively screen dictionary each time we explore a word

--- anagram.py
+++ anagram.py
@@ -12,25 +12,27 @@ class AnagramSolver:
 		self.solution = [] # set of anagram solutions, maybe shouldnt be an attribute
 
 
-	def screen_dict(self, inventory):
+	def screen_dict(self, dictionary, inventory):
 		"""
 		Screen all words in dictionary, removing those that
-		don't match the inventory at all. Mutates 
-		self.dictionary
+		don't match the inventory at all. Returns a 
+		screened dictionary
 		"""
-		for word, inv in self.dictionary.copy().items():
+		copy = dictionary.copy()
+		for word, inv in copy.copy().items(): # better way to do this?
 			if inventory.subtract(inv) == None:
-				del self.dictionary[word]
+				del copy[word]
+		return copy
 
 
 	def solve(self, inventory, max_words):
-		self.screen_dict(inventory)
+		screened = self.screen_dict(self.dictionary, inventory)
 		self.solution = [] # clear last solution
-		self.solve_helper(inventory, max_words, [])
+		self.solve_helper(screened, inventory, max_words, [])
 		return self.solution
 
 
-	def solve_helper(self, inventory, max_words, words):
+	def solve_helper(self, dictionary, inventory, max_words, words):
 		"""
 		Pass in a letter inventory and a number of
 		words to solve for. Returns a list of lists, 
@@ -47,11 +49,13 @@ class AnagramSolver:
 		elif max_words == 0: # base case 2: reached the word limit before running out of letters
 			return 
 		else: # recursive case: backtrack through all words in dictionary
-			for w, inv in self.dictionary.items():
+			screened = self.screen_dict(dictionary, inventory)
+			for w, inv in screened.items():
 				choose = inventory.subtract(inv)
 				if choose != None: # if we can subtract word from inventory
 					words.append(w)
-					self.solve_helper(choose, max_words - 1, words)
+					print(words)
+					self.solve_helper(screened, choose, max_words - 1, words)
 					words.pop()
 
 
